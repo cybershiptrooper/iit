b28a0bad22977c49db3860867b45833c3756994e
cybershiptrooper <cybershiptrooper@gmail.com>
2024-07-06 17:12:15 +0100

    bug fix for PR #11

--- tests/test_utils/caching_model_pair.py
+++ tests/test_utils/caching_model_pair.py
@@ -14,21 +14,13 @@ class CachingModelPair(StrictIITModelPair):
         ablation_x, ablation_y = ablation_input[0:2]
         base_x, base_y = base_input[0:2]
 
-        hl_model_input = base_x
-        try:
-            # Special check for some HL models that use intermediate variables (e.g. PVR)
-            if self.hl_model.uses_intermediate_variables():
-                hl_model_input = base_input
-        except AttributeError:
-            pass
-
-        hl_ablation_output, self.hl_cache = self.hl_model.run_with_cache(hl_model_input)
+        hl_ablation_output, self.hl_cache = self.hl_model.run_with_cache(ablation_input)
         ll_ablation_output, self.ll_cache = self.ll_model.run_with_cache(ablation_x)
 
         ll_nodes = self.corr[hl_node]
 
         hl_output = self.hl_model.run_with_hooks(
-            hl_model_input, fwd_hooks=[(hl_node.name, self.make_hl_ablation_hook(hl_node))]
+            base_input, fwd_hooks=[(hl_node.name, self.make_hl_ablation_hook(hl_node))]
         )
         ll_fwd_hooks = [
                 (ll_node.name, self.make_ll_ablation_hook(ll_node))
